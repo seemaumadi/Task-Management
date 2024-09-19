d96ed7cb42caa18ac7bc7ec649301119a1983bf8
seemaumadi <seemaumadi111@gmail.com>
2024-09-19 12:19:47 +0530

    Update settings.py

--- taskly/settings.py
+++ taskly/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
